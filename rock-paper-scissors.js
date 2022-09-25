f603c1307b5099a91be902b433a583bd2e15f2b0
Robert S <112721454+roberts-fullstack@users.noreply.github.com>
2022-09-25 19:17:12 -0400

    updates

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -8,22 +8,22 @@ let choice2 = prompt('Player 2, please choose rock, paper, or scissors: ');
 console.log(`Confirmed selection, Player 2: ${choice2}.`)
 
 if (choice1 === "rock" && choice2 === "paper"){
-  console.log('Paper beats Rock. Player 2 wins!');
+  console.log('Paper covers rock. Player 2 wins!');
 
 }else if (choice1 === "rock" && choice2 === "scissors"){
-console.log('Rock beats Scissors, Player 1 wins!'); 
+console.log('Rock smashes scissors, Player 1 wins!'); 
 
 }else if(choice1 === "paper" && choice2 === "rock"){
-  console.log('Paper beats rock, Player 1 wins!');
+  console.log('Paper covers rock, Player 1 wins!');
 
 }else if(choice1 === "paper" && choice2 === "scissors"){
-  console.log('Scissors beats Paper, Player 2 wins!');
+  console.log('Scissors cut paper, Player 2 wins!');
 
 }else if(choice1 === "scissors" && choice2 === "paper"){
-  console.log('Scissors beats Paper, Player 1 wins!')  
+  console.log('Scissors cut paper, Player 1 wins!')  
 
 }else if(choice1 === "scissors" && choice2 === "rock"){
-  console.log('Rock beates Scissors, Player 2 wins!')  
+  console.log('Rock smashes scissors, Player 2 wins!')  
 
 }else if(choice1 && choice2 === "rock"){
   console.log('The game is a tie!')
